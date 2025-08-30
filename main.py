1a64e45dc1e4c856e474d1c1bd4269d4324f47d9
Muhammad Derylian Khalfani <mderylkhalfani@gmail.com>
2025-08-30 16:07:47 +0700

    data trnsformation

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_data_transformation import DataTransformationTrainingPipeline
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+STAGE_NAME = 'Data Ingestion stage'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    logger.info(f'stage {STAGE_NAME} initiated')
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f'stage {STAGE_NAME} completed')
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data Transformation stage'
+
+try:
+    logger.info(f'stage {STAGE_NAME} initiated')
+    data_transformation_pipeline = DataTransformationTrainingPipeline()
+    data_transformation_pipeline.initiate_data_transformation()
+    logger.info(f'stage {STAGE_NAME} completed')
+
+except Exception as e:
+    logger.exception(e)
+    raise e
